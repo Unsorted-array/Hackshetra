e2cd5344f9cf6481d38d066b5e9fe6ec29b29b1c
pawandeep2007 <pawandeep2007@gmail.com>
2018-01-13 19:16:28 +0530

    view events

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+var events = require('./routes/events');
 
 var app = express();
 
@@ -27,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/login', login);
-
+app.use('/events', events);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
